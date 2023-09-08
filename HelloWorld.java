a425611b4dd78635f69a2bd6d14bc608a88e0f83
Joseph Dinh <joseph.dinh@mrjaffesclass.com>
2023-09-08 11:30:36 -0700

    Commit message

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,16 @@ public class HelloWorld
     public static void main(String[] args) 
     {
         System.out.println("Idontknow") ;
+        int i;
+        double f = 2.5;
+        boolean flag = true;
+        i = 10;
+        f += i;
+        flag = !flag;
+        i++;
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(flag);
     }
 }
 
