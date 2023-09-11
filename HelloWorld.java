d05992c872b1f053926427ddbf0fd3466ae6a279
Joseph Dinh <joseph.dinh@mrjaffesclass.com>
2023-09-11 11:40:31 -0700

    Commit message

--- HelloWorld.java
+++ HelloWorld.java
@@ -14,6 +14,25 @@ public class HelloWorld
         System.out.println(i);
         System.out.println(f);
         System.out.println(flag);
+        String str1 = "I am a string";
+        String str2 = str1;
+        String str3 = "I am also a string";
+        boolean stringsEqual = str1.equals(str3);
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str3);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 }
 
